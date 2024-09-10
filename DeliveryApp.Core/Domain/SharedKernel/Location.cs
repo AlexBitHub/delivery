3f170bdf49d968f5ea0769264ba847bd37ee1116
AlexBitHub <68807516+AlexBitHub@users.noreply.github.com>
2024-09-10 22:24:32 +0300

    Module-2
    * Разработан Value Object "Location"
    
    * Покрытие юнит тестами класса Location
    
    * Доработка класса Location

---
+++ DeliveryApp.Core/Domain/SharedKernel/Location.cs
@@ -0,0 +1,94 @@
+﻿using CSharpFunctionalExtensions;
+using Primitives;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DeliveryApp.Core.Domain.SharedKernel
+{
+    public class Location : ValueObject
+    {
+        /// <summary>
+        /// Минимально возможное значение координаты
+        /// </summary>
+        public const int MinValue = 2;
+
+        /// <summary>
+        /// Максимально возможное значение координаты
+        /// </summary>
+        public const int MaxValue = 10;
+
+        [ExcludeFromCodeCoverage]
+        private Location()
+        {
+        }
+
+        [ExcludeFromCodeCoverage]
+        private Location(int x, int y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+
+        /// <summary>
+        /// Создать координату
+        /// </summary>
+        /// <param name="x">Значение по горизонтали</param>
+        /// <param name="y">Значение по вертикали</param>
+        /// <returns></returns>
+        public static Result<Location, Error> Create(int x, int y)
+        {
+            if (x < MinValue || x > MaxValue) 
+            {
+                return new Error("coordinate.is.invalid", "Значение координаты X должно быть " +
+                                                          "не меньше 1.1 и не больше 10.10.");
+            }
+            if (y < MinValue || y > MaxValue)
+            {
+                return new Error("coordinate.is.invalid", "Значение координаты Y должно быть " +
+                                                          "не меньше 1.1 и не больше 10.10.");
+            }
+            return new Location(x, y);
+        }
+
+        /// <summary>
+        /// Создать рандомную координату
+        /// </summary>
+        /// <returns></returns>
+        public static Result<Location, Error> CreateRandomLocation()
+        {
+            var random = new Random();
+            int x = random.Next(MinValue, MaxValue);
+            int y = random.Next(MinValue, MaxValue);
+            return Create(x, y).Value;
+        }
+
+        /// <summary>
+        /// Расчет дистанции в шагах
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static int operator-(Location left, Location right)
+        {
+            var xDifference = Math.Abs(left.X - right.X);
+            var yDifference = Math.Abs(left.Y - right.Y);
+            return xDifference + yDifference;
+        }
+
+        [ExcludeFromCodeCoverage]
+        protected override IEnumerable<IComparable> GetEqualityComponents()
+        {
+            yield return X;
+            yield return Y;
+        }
+
+        private static bool ValidateCoordinate(int value)
+        {
+            if (value < MinValue || value > MaxValue)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/SharedKernel/LocationShould.cs
@@ -0,0 +1,82 @@
+﻿using DeliveryApp.Core.Domain.SharedKernel;
+using FluentAssertions;
+using Xunit;
+
+
+namespace DeliveryApp.UnitTests.Domain.SharedKernel
+{
+    public class LocationShould
+    {
+        [Theory]
+        [InlineData(2, 3)]
+        [InlineData(4, 5)]
+        [InlineData(6, 7)]
+        [InlineData(8, 9)]
+        [InlineData(10, 9)]
+        public void BeCorrectWhenCoordiantesAreCorrectOnCreate(int x, int y)
+        {
+            var location = Location.Create(x, y);
+
+            location.IsSuccess.Should().BeTrue();
+
+            location.Value.X.Equals(x);
+            location.Value.Y.Equals(y);
+        }
+
+        [Theory]
+        [InlineData(-1, 5)]
+        [InlineData(1, 11)]
+        [InlineData(20, 11)]
+        public void ReturnErrorWhenCoordinatesAreIncorrectOnCreate(int x, int y)
+        {
+            var location = Location.Create(x, y);
+
+            location.IsFailure.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(5, 8, 7, 2, 8)]
+        [InlineData(2, 9, 3, 6, 4)]
+        public void CalculateDistanceBetweenTwoInstance(int x1, int y1, int x2, int y2, int result)
+        {
+            var location1 = Location.Create(x1, y1).Value;
+            var location2 = Location.Create(x2, y2).Value;
+
+            var calcResult = location1 - location2;
+
+            calcResult.Should().Be(result);
+        }
+
+        [Fact]
+        public void BeEqualWhenAllPropertiesAreEqual()
+        {
+            var location1 = Location.Create(5, 4).Value;
+            var location2 = Location.Create(5, 4).Value;
+
+            var equalResult = location1 == location2;
+
+            equalResult.Should().BeTrue();
+        }
+
+        [Fact]
+        public void BeNotEqualWhenNotAllPropertiesAreEqual()
+        {
+            var location1 = Location.Create(5, 4).Value;
+            var location2 = Location.Create(2, 7).Value;
+
+            var equalResult = location1 == location2;
+
+            equalResult.Should().BeFalse();
+        }
+
+        [Fact]
+        public void CreateRandomLocation()
+        {
+            var randomLocation = Location.CreateRandomLocation();
+
+            randomLocation.IsSuccess.Should().BeTrue();
+            randomLocation.Value.X.Should().BeInRange(Location.MinValue, Location.MaxValue);
+            randomLocation.Value.Y.Should().BeInRange(Location.MinValue, Location.MaxValue);
+        }
+    }
+}
