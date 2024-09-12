8f30e9afb02dc659e07a337f0650180026701cd2
AlexeyBul <abulyg1n@yandex.ru>
2024-09-13 00:09:10 +0300

    module-3 Каркас юнит-тестов

---
+++ Tests/DeliveryApp.UnitTests/Domain/CourierAggregate/TransportShould.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.CourierAggregate
+{
+    public class TransportShould
+    {
+        //[Theory]
+        //[InlineData()]
+        public void CanBeFoundByName(string name, int id)
+        {
+
+        }
+
+        public void CanBeFoundById(int id, string name) 
+        {
+
+        }
+
+        [Fact]
+        public void BeEqualToTransportWithTheSameId()
+        {
+
+        }
+        
+        [Fact]
+        public void ReturnTypesNamesList()
+        {
+
+        }
+
+        public void ReturnErrorWhenTransportNotFoundByName(string name)
+        {
+
+        }
+
+        public void ReturnErrorWhenTransportNotFoundById(int id)
+        {
+
+        }
+    }
+}
