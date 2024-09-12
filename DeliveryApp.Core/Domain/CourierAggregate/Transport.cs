25abd9e143fc17469da5b98d8767201238eaf694
AlexeyBul <abulyg1n@yandex.ru>
2024-09-13 00:08:51 +0300

    module-3 Реализованы entity классы

---
+++ DeliveryApp.Core/Domain/CourierAggregate/Transport.cs
@@ -0,0 +1,89 @@
+﻿using CSharpFunctionalExtensions;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.CourierAggregate
+{
+    public class Transport : Entity<int>
+    {
+        public static readonly Transport Pedestrian = new Transport(1, nameof(Pedestrian).ToLowerInvariant(), 1);
+        public static readonly Transport Bicycle = new Transport(2, nameof(Bicycle).ToLowerInvariant(), 2);
+        public static readonly Transport Car = new Transport(3, nameof(Car).ToLowerInvariant(), 3);
+
+        private Transport() { }
+
+        private Transport(int id, string name, int speed) : this()
+        {
+            Id = id;
+            Name = name;
+            Speed = speed;
+        }
+
+        /// <summary>
+        /// Название транспорта
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Скорость транспорта
+        /// </summary>
+        public int Speed { get; }
+        
+
+        /// <summary>
+        /// Получить список видов транспорта
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Transport> List()
+        {
+            yield return Pedestrian;
+            yield return Bicycle;
+            yield return Car;
+        }
+
+        /// <summary>
+        /// Получить транспорт по имени
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static Result<Transport, Error> FromName(string name) 
+        {
+            var transport = List().SingleOrDefault(t => string.Equals(t.Name,
+                                                                      name,
+                                                                      StringComparison.CurrentCultureIgnoreCase));
+            if (transport == null) 
+            {
+                return Errors.StatusIsWrong();
+            }
+            return transport;
+        }
+
+        /// <summary>
+        /// Получить транспорт по Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static Result<Transport, Error> FromId(int id)
+        {
+            var transport = List().SingleOrDefault(t => t.Id == id);
+            if (transport == null)
+            {
+                return Errors.StatusIsWrong();
+            }
+            return transport;
+        }
+
+        public static class Errors
+        {
+            public static Error StatusIsWrong()
+            {
+                string message = $"Не верное значение. " +
+                                 $"Допустимые значения: {nameof(Transport).ToLowerInvariant()}: " +
+                                 $"{string.Join(",", List().Select(s => s.Name))}";
+
+                return new Error($"{nameof(Transport).ToLowerInvariant()}.is.wrong",
+                                 message);
+            }
+        }
+
+    }
+}
---
+++ DeliveryApp.Core/Domain/OrderAggregate/OrderStatus.cs
@@ -0,0 +1,83 @@
+﻿using CSharpFunctionalExtensions;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.OrderAggregate
+{
+    public class OrderStatus :Entity<int>
+    {
+        public static readonly OrderStatus Created = new OrderStatus(1, nameof(Created).ToLowerInvariant());
+        public static readonly OrderStatus Assigned = new OrderStatus(1, nameof(Assigned).ToLowerInvariant());
+        public static readonly OrderStatus Completed = new OrderStatus(1, nameof(Completed).ToLowerInvariant());
+        private OrderStatus() { }
+
+        private OrderStatus(int id, string name) : this()
+        {
+            Id = id;
+            Name = name;
+        }
+
+        /// <summary>
+        /// Название статуса заказа
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Список всех значений статусов
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<OrderStatus> List()
+        {
+            yield return Created;
+            yield return Assigned;
+            yield return Completed;
+        }
+
+        /// <summary>
+        /// Получить статус по названию
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static Result<OrderStatus, Error> FromName(string name)
+        {
+            var state = List().SingleOrDefault(s => string.Equals(s.Name, 
+                                                                  name, 
+                                                                  StringComparison.CurrentCultureIgnoreCase));
+            if (state is null) 
+            { 
+                return Errors.StatusIsWrong(); 
+            }
+            return state;
+        }
+
+        /// <summary>
+        /// Получить статус по идентификатору
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static Result<OrderStatus, Error> FromId(int id)
+        {
+            var state = List().SingleOrDefault(s => s.Id == id);
+            if (state is null)
+            {
+                return Errors.StatusIsWrong();
+            }
+            return state;
+        }
+
+        /// <summary>
+        /// Ошибки, которые может возвращать сущность
+        /// </summary>
+        public static class Errors
+        {
+            public static Error StatusIsWrong()
+            {
+                string message = $"Не верное значение. " +
+                                 $"Допустимые значения: {nameof(OrderStatus).ToLowerInvariant()}: " +
+                                 $"{string.Join(",", List().Select(s => s.Name))}";
+
+                return new Error($"{nameof(OrderStatus).ToLowerInvariant()}.is.wrong",
+                                 message);
+            }
+        }
+    }
+}
