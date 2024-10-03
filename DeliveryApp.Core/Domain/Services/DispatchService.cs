b39b95b0c767ceedf7d05e2196c7c0ecf8427ccf
AlexeyBul <abulyg1n@yandex.ru>
2024-10-03 22:07:37 +0300

    Разработка DispatchService

---
+++ DeliveryApp.Core/Domain/Services/DispatchService.cs
@@ -0,0 +1,26 @@
+﻿using DeliveryApp.Core.Domain.CourierAggregate;
+using DeliveryApp.Core.Domain.OrderAggregate;
+
+namespace DeliveryApp.Core.Domain.Services
+{
+    public class DispatchService : IDispatchService
+    {
+        public Courier Dispatch(Order order, List<Courier> couriers)
+        {
+            Courier fastestCourier = couriers.FirstOrDefault();
+            double fastestTime = double.MaxValue;
+
+            for (int i = 0; i < couriers.Count; i++)
+            {
+                var timeToOrder = couriers[i].CalculateTimeToDestionation(order.Location).Value;
+
+                if (timeToOrder < fastestTime)
+                {
+                    fastestCourier = couriers[i];
+                    fastestTime = timeToOrder;
+                }
+            }
+            return fastestCourier;
+        }
+    }
+}
---
+++ DeliveryApp.Core/Domain/Services/IDispatchService.cs
@@ -0,0 +1,10 @@
+﻿using DeliveryApp.Core.Domain.CourierAggregate;
+using DeliveryApp.Core.Domain.OrderAggregate;
+
+namespace DeliveryApp.Core.Domain.Services
+{
+    public interface IDispatchService
+    {
+        Courier Dispatch(Order order, List<Courier> couriers);
+    }
+}
